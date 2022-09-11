a0d2614fac03a960f0f9891fb53cd5c7cf5fcece
mscMstr <delgadocarla2021@gmail.com>
2022-09-11 19:25:04 -0400

    initial commit

---
+++ src/columns.js
@@ -0,0 +1,30 @@
+export const COLUMNS = [
+    {
+        Header: 'Run',
+        accessor: 'run'
+    },
+    {
+        Header: 'Run Relief',
+        accessor: 'run_relief'
+    },
+    {
+        Header: 'Bus',
+        accessor: 'bus'
+    },
+    {
+        Header: 'Time Due',
+        accessor: 'time_due'
+    },
+    {
+        Header: 'Expected',
+        accessor: 'expected'
+    },
+    {
+        Header: 'Recommended',
+        accessor: 'recommended'
+    },
+    {
+        Header: 'Executed',
+        accessor: 'exectuted'
+    }
+]
