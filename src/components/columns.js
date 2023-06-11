a6ebd28f4064568f5fc85a516c7b614305cc8a10
joerovar97 <joseph.rodriguez.v@gmail.com>
2023-06-11 11:57:54 -0500

    changing formats

--- src/components/columns.js
+++ src/components/columns.js
@@ -13,78 +13,78 @@ export const COLUMNS = [
         accessor: "op_id",
     },
     {
-        Header: 'Off',
+        Header: 'Missing',
         accessor: "off",
     },
     {
         Header: "Time Due",
         accessor: "s_time",
     },
-    {
-        Header: "Sched. Intervals (min)",
-        accessor: "s_hws",
-    },
+    // {
+    //     Header: "Sched. Intervals (min)",
+    //     accessor: "s_hws",
+    // },
     {
         Header: 'Predicted Time',
         accessor: 'p_time',
     },
-    {
-        Header: 'Pred. Intervals (min)',
-        accessor: 'p_hws',
-    },    
-    {
-        Header: 'Pred. Board',
-        accessor: 'p_board',
-    },
-    {
-        Header: 'Pred. Sched. Dev (min)',
-        accessor: 'p_dev',
-    },
-    {
-        Header: 'Rec. Action',
-        accessor: 'r_act',
-    },
+    // {
+    //     Header: 'Pred. Intervals (min)',
+    //     accessor: 'p_hws',
+    // },    
+    // {
+    //     Header: 'Pred. Board',
+    //     accessor: 'p_board',
+    // },
+    // {
+    //     Header: 'Pred. Sched. Dev (min)',
+    //     accessor: 'p_dev',
+    // },
+    // {
+    //     Header: 'Rec. Action',
+    //     accessor: 'r_act',
+    // },
     {
         Header: 'Rec. Time',
         accessor: 'r_time',
-    },
-    {
-        Header: 'Rec. Intervals (min)',
-        accessor: 'r_hws',
-    },
-    {
-        Header: 'Rec. Sched. Dev (min)',
-        accessor: 'r_dev',
-    },    
-    {
-        Header: 'Rec. Board',
-        accessor: 'r_board',
-    },    
-    {
-        Header: 'Rec. RL Time',
-        accessor: 'rl_time',
-    },
-
-    {
-        Header: 'Act. Action',
-        accessor: 'a_act',
-    },    
-
-    {
-        Header: 'Act. Time',
-        accessor: 'a_time',
-    },
-    {
-        Header: 'Act. Intervals (min)',
-        accessor: 'a_hws',
-    },
-    {
-        Header: 'Act. Sched. Dev (min)',
-        accessor: 'a_dev',
-    },
-    {
-        Header: 'Mid holding (min)',
-        accessor: 'mid_ht',
     }
+    // {
+    //     Header: 'Rec. Intervals (min)',
+    //     accessor: 'r_hws',
+    // },
+    // {
+    //     Header: 'Rec. Sched. Dev (min)',
+    //     accessor: 'r_dev',
+    // },    
+    // {
+    //     Header: 'Rec. Board',
+    //     accessor: 'r_board',
+    // },    
+    // {
+    //     Header: 'Rec. RL Time',
+    //     accessor: 'rl_time',
+    // },
+
+    // {
+    //     Header: 'Act. Action',
+    //     accessor: 'a_act',
+    // },    
+
+    // {
+    //     Header: 'Act. Time',
+    //     accessor: 'a_time',
+    // },
+    // {
+    //     Header: 'Act. Intervals (min)',
+    //     accessor: 'a_hws',
+    // },
+    // {
+    //     Header: 'Act. Sched. Dev (min)',
+    //     accessor: 'a_dev',
+    // },
+    // {
+    //     Header: 'Mid holding (min)',
+    //     accessor: 'mid_ht',
+    // }
 
 ]
